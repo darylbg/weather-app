8901d61443cd70a19c9a2085bf70fd550b26c7a4
darylbg <darylxcuf@gmail.com>
2023-03-08 13:24:59 +0000

    added dynamic date loading in js/index.html the api key is removed in this commit as i have not figured out how to hide it properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
 var button = $('#search-btn');
 var value = $('#example-search-input');
 
+var now = dayjs();
+$('#current-day-date').text(now.format("MM-DD-YYYY"));
+
+function setForecastDates() {
+    var day1Date = now.add(1, 'day');
+    var day2Date = now.add(2, 'day');
+    var day3Date = now.add(3, 'day');
+    var day4Date = now.add(4, 'day');
+    var day5Date = now.add(5, 'day');
+
+    $('#day1-date').text(day1Date.format("MM-DD-YYYY"));
+    $('#day2-date').text(day2Date.format("MM-DD-YYYY"));
+    $('#day3-date').text(day3Date.format("MM-DD-YYYY"));
+    $('#day4-date').text(day4Date.format("MM-DD-YYYY"));
+    $('#day5-date').text(day5Date.format("MM-DD-YYYY"));
+} setForecastDates();
+
 button.click(function() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + value[0].value + '&appid=1f8683fe1384aa1e2f6cda5b83b27321')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + value[0].value + '&appid=')
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
                 console.log(data.city.name);
-                setCurrentWeather(data.list[0].weather[0].icon, data.city.name, data.list[0].dt_txt, data.list[0].main.temp, data.list[0].weather[0].description, data.list[0].wind.speed);
+                setCurrentWeather(data.list[0].weather[0].icon, data.city.name, data.list[0].main.temp, data.list[0].weather[0].description, data.list[0].wind.speed);
                 console.log(data.list[0].weather[0].icon);
                 //$('#current-weather').find('h5').text(data.city.name);
-                setFutureWeather(data.list[6].dt_txt);
+                //setFutureWeather(data.list[6].dt_txt);
+                //setForecastDates();
             });
         } else {
             alert('error');
@@ -22,20 +40,14 @@ button.click(function() {
     });
 });
 
-function setCurrentWeather(descriptionIcon, cityName, date, temperature, weather, windSpeed) {
+function setCurrentWeather(descriptionIcon, cityName, temperature, weather, windSpeed) {
     $('#current-weather').css('background-image', 'url("https://openweathermap.org/img/w/' + descriptionIcon + '.png")');
-    $('#current-weather').find('h5').text(cityName);
-    $('#current-weather').find('h5').eq(1).text(date);
-    $('#current-weather').find('p').eq(0).text((temperature - 273.15).toFixed(1) + '°C');
+    $('#current-weather').find('h5').eq(0).text(cityName);
+    $('#current-weather').find('p').eq(0).text((temperature - 273.15).toFixed(2) + '°C');
     $('#current-weather').find('p').eq(1).text(weather);
-    $('#current-weather').find('p').eq(2).text('Wind Speed: ' + (windSpeed * 2.2).toFixed(1) + 'mph');
+    $('#current-weather').find('p').eq(2).text('Wind Speed: ' + (windSpeed * 2.2).toFixed(2) + 'mph');
 }
 
-function setFutureWeather(date) {
-    var futureWeatherEl = $('.future-weather-display').children();
-    console.log(futureWeatherEl.length);
+function setFutureWeather() {
 
-    for (i = 0; i < futureWeatherEl.length; i++) {
-        $(futureWeatherEl[i]).children().children('h5').text(date);
-    }
 }
